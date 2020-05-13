119ccb14841f67f3005460983b4158a54420f9d1
Vinicius Holanda <holanda796@gmail.com>
2020-05-13 20:36:52 -0300

    Add bmi calculator node.js

--- calculator.js
+++ calculator.js
@@ -5,24 +5,37 @@ const app = express()
 
 const port = 3000
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true})) //necessario para usar body parser
 
 
 app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html')
+ res.sendFile(__dirname + '/index.html') //dirname acha o diretorio
 })
 
 app.post("/", function(req, res) {
 
-var num1 = Number(req.body.n1)
-var num2 = Number(req.body.n2)
+var num1 = Number(req.body.n1) //Number é para trabalhar com numeros, sem ele o valor vira string
+var num2 = Number(req.body.n2) 
 
 var result = num1 + num2
 
   res.send("The result is " + result)
 })
 
+app.get('/bmicalculator', function(req, res){
+  res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
+app.post('/bmicalculator', function(req, res){
+
+let weight = parseFloat(req.body.weight) 
+let height = parseFloat(req.body.height)
+
+let bmi = weight / (height * height)
+
+res.send("Your BMI is " + bmi)
+
+})
+
 app.listen(port, () => console.log(`Example app listening on port 
 !`))
-
-
